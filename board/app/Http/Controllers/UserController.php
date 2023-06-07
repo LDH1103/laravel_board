c609441184023ede2de98b04d907937e4cadb549
ldh1103 <ldh517525@gmail.com>
2023-06-07 17:37:30 +0900

    23-06-07

--- board/app/Http/Controllers/UserController.php
+++ board/app/Http/Controllers/UserController.php
@@ -36,23 +36,23 @@ class UserController extends Controller
         // Log::info('info', $arr);
         // Log::debug('debug', $arr);
         // Log::debug('debug');
-        // $ch = curl_init();
-        // // $url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList'; /*URL*/
-        // $url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList'; /*URL*/
-        // $queryParams = '?' . urlencode('serviceKey') . '=q1Huc9EjZjvBYP%2BNKi0ILB%2FS%2BhmYkimR2o%2FIfQey1bl0NGsyoDHQJVnSYSEwPfvS9C9SqZkaD%2FXMw9SLRkLlqA%3D%3D'; /*Service Key*/
+        $ch = curl_init();
+        // $url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList'; /*URL*/
+        $url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList'; /*URL*/
+        $queryParams = '?' . urlencode('serviceKey') . '=q1Huc9EjZjvBYP%2BNKi0ILB%2FS%2BhmYkimR2o%2FIfQey1bl0NGsyoDHQJVnSYSEwPfvS9C9SqZkaD%2FXMw9SLRkLlqA%3D%3D'; /*Service Key*/
 
-        // curl_setopt($ch, CURLOPT_URL, $url . $queryParams);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        // curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        // $response = curl_exec($ch);
-        // $result = simplexml_load_string($response);
-        // $a = json_encode($result, JSON_UNESCAPED_UNICODE);
+        curl_setopt($ch, CURLOPT_URL, $url . $queryParams);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        $response = curl_exec($ch);
+        $result = simplexml_load_string($response);
+        $a = json_encode($result, JSON_UNESCAPED_UNICODE);
 
-        // curl_close($ch);
+        curl_close($ch);
 
-        // return print_r($a);
-        return view('login');
+        return print_r($a);
+        // return view('login');
     }
     
     public function loginpost(Request $req) {
@@ -129,7 +129,7 @@ class UserController extends Controller
                 ->route('users.registration')
                 ->with('error', $error);
         }
-            
+
         // Mail::to($user)->send(new SendEmail($user));
         
         // 회원가입 완료 후 로그인 페이지로 이동
