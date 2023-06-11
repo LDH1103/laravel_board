187b75893a5dadc438a1109a18aeb43143585e42
LDH1103 <ldh517525@gmail.com>
2023-06-11 19:37:56 +0900

    23-06-11

--- board/app/Http/Controllers/UserController.php
+++ board/app/Http/Controllers/UserController.php
@@ -37,8 +37,8 @@ class UserController extends Controller
         // Log::debug('debug', $arr);
         // Log::debug('debug');
         $ch = curl_init();
-        // $url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList'; /*URL*/
-        $url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList'; /*URL*/
+        $url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList'; /*URL*/
+        // $url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList'; /*URL*/
         $url = 'http://openapi.airport.co.kr/service/rest/FlightScheduleList/getDflightScheduleList';
         $queryParams = '?' . urlencode('serviceKey') . '=q1Huc9EjZjvBYP%2BNKi0ILB%2FS%2BhmYkimR2o%2FIfQey1bl0NGsyoDHQJVnSYSEwPfvS9C9SqZkaD%2FXMw9SLRkLlqA%3D%3D'; /*Service Key*/
 
