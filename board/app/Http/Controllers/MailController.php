6ef695065161b39ad46b21ef3f9cb1926f5b876c
LDH1103 <ldh517525@gmail.com>
2023-06-30 19:50:11 +0900

    23-06-30

--- board/app/Http/Controllers/MailController.php
+++ board/app/Http/Controllers/MailController.php
@@ -15,7 +15,6 @@ class MailController extends Controller
     public function mailpost(Request $req) {
         // return var_dump($req->mailAddress);
         Mail::to($req->mailAddress)->send(new SendEmail($req->mailAddress));
-        
         return '전송 완료';
         // return redirect()->back()->with('success', '메일 전송 완료');
     }
